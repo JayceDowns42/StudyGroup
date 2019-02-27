79621dddd46de376318a5fdf4d7b021cc1294b87
WalkerTxsRngr7 <walkergross7@gmail.com>
2019-02-27 16:17:43 -0600

    problems with order summary

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
-order = document.getElementById('Order');
+let order = document.getElementById('Order');
 order.addEventListener("click", summary);
 
+var top = document.getElementById('top').option;
+var sum = document.getElementById('summary');
+
+
+
+
 function summary(){
-    document.write(`<h3>Name: ${name}</h3>`);
-    document.write(`<h3>Name: ${phone}</h3>`);
-    document.write(`<h3>Name: ${qty}</h3>`);
-    document.write(`<h3>Name: ${top}</h3>`);
+    sum.innerHTML += `<h3>Name: ${top}</h3>`;
+    // document.write(`<h3>Name: ${phone}</h3>`);
+    // document.write(`<h3>Name: ${qty}</h3>`);
+    // document.write(`<h3>Name: ${top}</h3>`);
 }
+document.get
