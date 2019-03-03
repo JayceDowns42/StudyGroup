409f80a0d5c948f590929af7b34070eef18c2733
WalkerTxsRngr7 <walkergross7@gmail.com>
2019-03-03 15:03:57 -0600

    summary implemented correctly

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
 let order = document.getElementById('Order');
 order.addEventListener("click", summary);
 
-var top = document.getElementById('top').option;
+//var names have to be different than id if not a number!!! 
+var nameVar = document.getElementById('name');
+var phone = document.getElementById('phone');
+var qty = document.getElementById('qty');
+var tops = document.getElementById('top');
+
 var sum = document.getElementById('summary');
 
 
-
-
 function summary(){
-    sum.innerHTML += `<h3>Name: ${top}</h3>`;
-    // document.write(`<h3>Name: ${phone}</h3>`);
-    // document.write(`<h3>Name: ${qty}</h3>`);
-    // document.write(`<h3>Name: ${top}</h3>`);
+    sum.innerHTML += `<h3>Name: ${nameVar.value}</h3>`;
+    sum.innerHTML += `<h3>Phone: ${phone.value}</h3>`;
+    sum.innerHTML += `<h3>Quantity: ${qty.value}</h3>`;
+    sum.innerHTML += `<h3>Topping: ${tops.value}</h3>`;
+  
 }
-document.get
